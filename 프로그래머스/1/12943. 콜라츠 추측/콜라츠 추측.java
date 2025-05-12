e7e8e9f01235582cf8e0badcc2cfbb520eb82b8f
Syuare <tachunsa13@gmail.com>
2025-05-12 09:57:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long result = num;
+        
+        for(int i=1; i<=500; i++){
+            if(result != 1) {
+                if(result%2==0) {
+                   result = result/2;
+                    answer++;
+                } else {
+                  result = result*3+1;        
+                    answer++;
+                }    
+            }
+        }
+        
+        if (answer == 1) return 0;
+        else if (answer == 500) return -1;
+        else return answer;
+    }
+}
