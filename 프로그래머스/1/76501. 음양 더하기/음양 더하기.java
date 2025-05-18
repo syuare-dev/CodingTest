a42b8c5819171d90321ce8111c31f5a6a0552dd3
Syuare <tachunsa13@gmail.com>
2025-05-18 13:57:35 +0900

    [level 1] Title: 음양 더하기, Time: 3.64 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,16 +1,21 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         
-        for (int i=0; i<signs.length; i++) {
-            if (!signs[i]) absolutes[i] = -absolutes[i];
-        }
+//         for (int i=0; i<signs.length; i++) {
+//             if (!signs[i]) absolutes[i] = -absolutes[i];
+//         }
         
-        int answer = 0;
-        for (int a : absolutes) {
-            answer += a;
-        }
+//         int answer = 0;
+//         for (int a : absolutes) {
+//             answer += a;
+//         }
         
+//         return answer;
         
-        return answer;
+        return IntStream.range(0, absolutes.length)
+            .map(i-> signs[i] ? absolutes[i] : -absolutes[i])
+            .sum();
     }
 }
