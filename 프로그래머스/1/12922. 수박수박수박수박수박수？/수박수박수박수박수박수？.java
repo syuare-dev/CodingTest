89d47b62e87bde5361d1fbcf1851ca9f4f3e7ecf
Syuare <tachunsa13@gmail.com>
2025-05-22 09:50:37 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 46.90 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -4,13 +4,9 @@ class Solution {
         String answer = "";
 
         for (int i=0; i<n; i++) {
-            if (answer.length()%2 == 0) {
-                answer += "수";
-            } else {
-                answer += "박";
-            }
+            answer = (answer.length()%2 == 0) ? answer+"수" : answer+"박";
         }
-        
+
         return answer;
     }
 }
