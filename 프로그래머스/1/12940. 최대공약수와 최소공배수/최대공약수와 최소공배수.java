fd48576c53cd905907d024d22e81464d63016862
Syuare <tachunsa13@gmail.com>
2025-05-29 16:01:31 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.22 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+
+        int gcd = 0;
+
+        for (int i=1; i<Math.max(n,m); i++) {
+            if(n%i==0 && m%i == 0) {
+                gcd = i;
+                answer[0] = gcd;
+            }
+        }
+
+        answer[1] = (n * m)/gcd;
+
+        return answer;
+    }
+}
